77445e04e13919da5647051a6bb5817c6a588426
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-08-06 23:26:00 +0200

    MAINT: address Ruff issues

--- src/sphinx_api_relink/linkcode.py
+++ src/sphinx_api_relink/linkcode.py
@@ -135,7 +135,7 @@ def get_blob_url(github_repo: str) -> str:
 
 @lru_cache(maxsize=1)
 def _get_commit_sha() -> str:
-    result = subprocess.run(
+    result = subprocess.run(  # noqa: S603
         ["git", "rev-parse", "HEAD"],  # noqa: S607
         capture_output=True,
         check=True,
@@ -147,7 +147,7 @@ def _get_commit_sha() -> str:
 
 def _get_latest_tag() -> str | None:
     try:
-        result = subprocess.check_output(
+        result = subprocess.check_output(  # noqa: S603
             ["git", "describe", "--tags", "--exact-match"],  # noqa: S607
             stderr=subprocess.PIPE,
             universal_newlines=True,
